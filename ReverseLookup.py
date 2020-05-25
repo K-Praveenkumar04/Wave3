cbc4c577581ee79085fc5a3f2822cb1baeb54c9f
K-Praveenkumar04 <Kevin.Praveenkumar22@ycdsbk12.ca>
2020-05-25 00:13:06 -0400

    editing reverse lookup

--- ReverseLookup.py
+++ ReverseLookup.py
@@ -6,13 +6,14 @@ def reverseLookup(data, value):
             keys.append(key)
     return keys
 def main():
-    antonyms = {"sad" : "happy", "left" : "right", "stop" : "go", "sweet" : "sour"}
-    print("The antonym for 'sad' are: ", reverseLookup(antonyms, "sad"))
-    print("happy")
-    print()
-    print("The antonym  for 'left' is: ", reverseLookup(antonyms, "left"))
-    print("right")
-    print("The antonym  for 'stop' is: ", reverseLookup(antonyms, "stop"))
-    print("go")
+    antonyms = {"sad" : "happy", "sad" : "cheerful", "left" : "right", "stop" : "go", "sweet" : "sour"}
+    print("The antonym for 'left' is: ", reverseLookup(antonyms, "left"))
+    print("[right]")
+    print("The antonyms for 'sad' are: ", reverseLookup(antonyms, "sad"))
+    print("[happy, cheerful]")
+    print("The antonym for 'stop' is: ", reverseLookup(antonyms, "stop"))
+    print("[go]")
+    print("The antonym for 'kevin' is: "), reverseLookup(antonyms, "kevin")
+    print(" [--]")
 if __name__ == "__main__":
     main()
