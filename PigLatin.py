421b382e9236301242fe10d48d4bdbb95b01d629
K-Praveenkumar04 <Kevin.Praveenkumar22@ycdsbk12.ca>
2020-06-16 17:53:08 -0400

    updating pig latin

--- PigLatin.py
+++ PigLatin.py
@@ -1,7 +1,15 @@
 #Pig Latin
-ay = 'ay'
-way = 'way'
-consonant = ('B','C','D','F','G','H','J','K','L','M','N','P','Q','R','S','T','Y','V','X','Z')
-vowel = ('A','E','I','O','U')
-pig_latin_string =''
-user_sentenc
+b = input("Enter a word to convert to pig latin: ")
+def piglatin(word):
+    for char in range(0, len(word)):
+        b = word[char]
+        if char == 0 and (b == "a" or b == "e" or b == "i" or b == "o" or b == "u"):
+            word = word + "way"
+            break
+        elif b == "a" or (b == "e" or b == "i" or b == "0" or b == "u"):
+            save = word[:char]
+            word = word[char:] + save + "ay"
+            break
+    return word
+print(piglatin(a))
+
